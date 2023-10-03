bf06382eeb3160f95b123dda47d14119a35b01f5
isabelgarcesg <65743747+isabelgarcesg@users.noreply.github.com>
2023-10-02 21:02:45 -0500

    Reto

---
+++ src/router/index.js
@@ -0,0 +1,43 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import CrearView from '../components/CrearView.vue'
+import ListarView from '../components/ListarView.vue'
+import EditarView from '../components/EditarView.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/crear',
+    name: 'crear',
+    component: CrearView
+  },
+  {
+    path: '/listar',
+    name: 'lista',
+    component: ListarView
+  },
+  {
+    path: '/editar/:id',
+    name: 'editar',
+    component: EditarView
+  },
+  {
+    path: '/about',
+    name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
