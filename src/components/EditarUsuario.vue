3ecb64a3760846ef13b97e38862ee00c4a211e19
Mpaulinatp <mpaulis.tobon@gmail.com>
2023-10-09 21:13:04 -0500

    funciona entidad

--- src/components/EditarUsuario.vue
+++ src/components/EditarUsuario.vue
@@ -3,36 +3,38 @@
 
         <div class="card">
             <div class="card-header">
-                <b>Edicion de usuario</b>
+                <b>Creación de usuario</b>
             </div>
             <div class="card-body">
-                <!--v-on:submit.prevent="edicionUsuarios"-->
-                <form v-on:submit.prevent="edicionUsuarios">
+                <!--v-on:submit.prevent="edicionusuarios"-->
+                <form v-on:submit.prevent="edicionusuarios">
 
+                    <div class="form-group">
+                        <label for="name">Nombre de la usuario</label>
+                        <input type="text" class="form-control" name="name" v-model="usuario.name" id="name"
+                            aria-describedby="helpId" placeholder="Nombre">
+                        <small id="helpId" class="form-text text-muted">Ingrese el nombre de la usuario</small>
+                    </div>
 
+                    <div class="form-group">
+                        <label for="phone">Teléfono</label>
+                        <input type="text" class="form-control" name="phone" v-model="usuario.phone" id="phone"
+                            aria-describedby="helpId" placeholder="Teléfono">
+                        <small id="helpId" class="form-text text-muted">Ingrese el teléfono de la usuario</small>
+                    </div>
+
+                    <!-- <div class="form-group">
+                      <label for="adress">Dirección</label>
+                      <input type="text"
+                        class="form-control" name="adress" v-model="usuario.adress" id="adress" aria-describedby="helpId" placeholder="Dirección">
+                      <small id="helpId" class="form-text text-muted">Ingrese la dirección de la usuario</small>
+                    </div> -->
                     <div class="form-group">
                         <label for="doc">Documento de identidad</label>
                         <input type="text" class="form-control" name="doc" v-model="usuario.doc" id="doc"
                             aria-describedby="helpId" placeholder="ID">
                         <small id="helpId" class="form-text text-muted">Ingrese el documento del usuario</small>
                     </div>
-
-
-                    <div class="form-group">
-                        <label for="name">Nombre completo</label>
-                        <input type="text" class="form-control" name="name" v-model="usuario.name" id="name"
-                            aria-describedby="helpId" placeholder="Nombre">
-                        <small id="helpId" class="form-text text-muted">Ingrese el nombre del usuario</small>
-                    </div>
-
-
-                    <div class="form-group">
-                        <label for="phone">Celular</label>
-                        <input type="text" class="form-control" name="phone" v-model="usuario.phone" id="phone"
-                            aria-describedby="helpId" placeholder="Número de celular">
-                        <small id="helpId" class="form-text text-muted">Ingrese el celular del usuario</small>
-                    </div>
-
 
                     <div class="form-group">
                         <label for="password">Contraseña</label>
@@ -41,22 +43,12 @@
                         <small id="helpId" class="form-text text-muted">Ingrese la contraseña</small>
                     </div>
 
-
                     <div class="form-group">
-                        <label for="position">Position</label>
+                        <label for="position">Cargo</label>
                         <input type="text" class="form-control" name="position" v-model="usuario.position" id="position"
                             aria-describedby="helpId" placeholder="Position">
                         <small id="helpId" class="form-text text-muted">Ingrese el position wtf?</small>
                     </div>
-
-
-                    <!-- <div class="form-group">
-                      <label for="userType">Tipo de usuario</label>
-                      <input type="int"
-                        class="form-control" name="userType"  v-model="usuario.userType" id="userType" aria-describedby="helpId" placeholder="">
-                      <small id="helpId" class="form-text text-muted">Ingrese el perfil del usuario</small>
-                    </div>   -->
-
 
                     <div class="form-group">
                         <label for="userType">Tipo de usuario</label>
@@ -69,15 +61,6 @@
                         <small id="helpId" class="form-text text-muted">Seleccione el perfil del usuario</small>
                     </div>
 
-                    <!-- Puede que este se elimine y se deje como un ID de la entidas fijo si solo tenemos que hacer un hospital, depende. -->
-                    <!-- <div class="form-group">
-                        <label for="entityID">ID del prestador de servicios</label>
-                        <input type="int" class="form-control" name="entityID" v-model="usuario.entityID" id="entityID"
-                            aria-describedby="helpId" placeholder="">
-                        <small id="helpId" class="form-text text-muted">Ingrese el ID de la entidad/hospital</small>
-                    </div> -->
-
-                    <!-- BOTONES -->
                     <div class="btn-group" role="group" aria-label="">
                         <button type="submit" class="btn btn-success">Modificar</button>
                         <button type="reset" class="btn btn-warning">Cancelar</button>
@@ -86,17 +69,12 @@
 
                 </form>
             </div>
-
-
             <div class="card-footer text-muted">
                 Copyright: LifeTech Solutions 2023
             </div>
-
-
         </div>
     </div>
 </template>
-
 
 <script>
 export default {
@@ -106,60 +84,33 @@ export default {
         }
     },
     created: function () {
-        //    //this.obtenerInformacionID();
-        this.edicionUsuarios();
+        this.edicionusuarios();
     },
     methods: {
-
-        obtenerInformacionID() {
-            fetch('https://redb.qsystems.co/QS3100/QServlet?operation=queryUserByEntity&tna=6&userEntityId=82&key=11e2e476-717b-4898-ac02-693abdecdc9b' + this.$route.params.id)
-                .then(respuesta => respuesta.json()) //es como un return y también hago una función arrow para volver la respuesta un json
-                .then((datosRespuesta) => {
-                    console.log(datosRespuesta)
-                    this.paciente = datosRespuesta[0];
-
-                })
-                .catch(console.log) //es como try except
-
-        },
-        edicionUsuarios() {
+        edicionusuarios() {
             let operation = "UpdateUser"
             let tna = 6
-            // let userId=this.usuario.id
-            // let nameUser=this.usuario.name
-            // let phoneUser=this.usuario.phone
-            // let passwordUser=this.usuario.password
-            // let documentUser=this.usuario.doc
-            // let positionUser=this.usuario.position
+            let userId = this.$route.params.id
+            let key = "11e2e476-717b-4898-ac02-693abdecdc9b"
+            let nameUser = this.usuario.name
+            let phoneUser = this.usuario.phone
+            let positionUser = this.usuario.position
+            let passwordUser = this.usuario.password
+            let documentUser = this.usuario.doc
             let UserType = 3; // Declarar UserType fuera de los bloques if
             if (this.usuario.userType === "Administrador") {
                 UserType = 1;
             } else if (this.usuario.userType === "Auditor") {
                 UserType = 2;
             }
-            //let userEntityId=82
-            let key = "11e2e476-717b-4898-ac02-693abdecdc9b"
-            let datosEnviar = {
-                userId: this.$route.params.id, nameUser: this.usuario.name,
-                phoneUser: this.usuario.phone,
-                passwordUser: this.usuario.password,
-                documentUser: this.usuario.doc,
-                positionUser: this.usuario.position,
-                userEntityId: 82
-            }
-            fetch('https://redb.qsystems.co/QS3100/QServlet?operation=' + operation + '&tna=' + tna + '&key=' + key + '&UserType=' + UserType, {
-                method: "POST",
-                body: JSON.stringify(datosEnviar)
-            })
+            fetch('https://redb.qsystems.co/QS3100/QServlet?operation=' + operation + '&tna=' + tna + '&nameUser=' + nameUser + '&phoneUser=' + phoneUser + '&positionUser=' + positionUser + '&userId=' + userId + +'&documentUser=' + documentUser + '&userType=' + UserType + '&passwordUser' + passwordUser + '&key=' + key)
                 .then(respuesta => respuesta.json())
                 .then((datosRespuesta) => {
                     console.log(datosRespuesta)
-                    //window.location.href='../ListarUsuario'
+                    //window.location.href='../Listarusuario'
                 })
-
+            //window.location.href='../Listarusuario'
         },
     }
-
-
 }
 </script>
